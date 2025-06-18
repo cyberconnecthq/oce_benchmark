690134d45fbee338a365bfd0a77974be8f76e4d7
Zhejian <zhejian@ZhejiandeMacBook-Pro.local>
2025-06-18 14:59:15 +0800

    add

---
+++ dataset/tasks/268509ac-c7db-47f5-913b-c5faf5e408e8/validate.py
@@ -0,0 +1,49 @@
+from web3 import Web3, HTTPProvider
+
+from dataset.constants import WETH_CONTRACT_ADDRESS_ETH, BNB_CONTRACT_ADDRESS_ETH
+
+# Ethereum RPC URL (local or public node)
+ETH_RPC_URL = "http://127.0.0.1:8545"
+
+# Default private key (for local testing only)
+PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+
+# ERC20 ABI (only balanceOf method)
+ERC20_ABI = [
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function"
+    }
+]
+
+# Initialize web3 object
+w3_eth = Web3(HTTPProvider(ETH_RPC_URL))
+
+# Get account address
+account = w3_eth.eth.account.from_key(PRIVATE_KEY)
+addr = account.address
+
+# Contract instances
+weth_contract = w3_eth.eth.contract(address=Web3.to_checksum_address(WETH_CONTRACT_ADDRESS_ETH), abi=ERC20_ABI)
+bnb_contract = w3_eth.eth.contract(address=Web3.to_checksum_address(BNB_CONTRACT_ADDRESS_ETH), abi=ERC20_ABI)
+
+async def get_balances():
+    # ETH balance
+    eth_balance = w3_eth.eth.get_balance(addr)
+    # WETH balance
+    weth_balance = weth_contract.functions.balanceOf(addr).call()
+    # BNB balance
+    bnb_balance = bnb_contract.functions.balanceOf(addr).call()
+    return (
+        f"Ethereum Mainnet:\n"
+        f"ETH Balance: {eth_balance / 10**18} ETH\n"
+        f"WETH Balance: {weth_balance / 10**18} WETH\n"
+        f"BNB Balance: {bnb_balance / 10**18} BNB\n"
+    )
+
+if __name__ == '__main__':
+    import asyncio
+    print(asyncio.run(get_balances()))
---
+++ dataset/tasks/a8e42738-f753-4d87-8309-3e1b0bf6f8c1/validate.py
@@ -0,0 +1,42 @@
+from web3 import Web3, HTTPProvider
+from eth_account.signers.local import LocalAccount
+from dataset.constants import WETH_CONTRACT_ADDRESS_ETH
+
+RPC_URL = "http://127.0.0.1:8545"
+PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"  # default anvil account
+w3 = Web3(HTTPProvider(RPC_URL))
+account: LocalAccount = w3.eth.account.from_key(PRIVATE_KEY)
+addr = account.address
+
+# Uniswap V3 Router address (mainnet)
+UNISWAP_V3_ROUTER = Web3.to_checksum_address("0xE592427A0AEce92De3Edee1F18E0157C05861564")
+WETH = Web3.to_checksum_address(WETH_CONTRACT_ADDRESS_ETH)
+
+# ERC20 ABI with allowance function
+ERC20_ABI = [
+    {
+        "constant": True,
+        "inputs": [
+            {"name": "_owner", "type": "address"},
+            {"name": "_spender", "type": "address"}
+        ],
+        "name": "allowance",
+        "outputs": [{"name": "", "type": "uint256"}],
+        "type": "function"
+    }
+]
+
+# Get WETH contract instance
+weth_contract = w3.eth.contract(address=WETH, abi=ERC20_ABI)
+
+async def get_balances():
+    # Query WETH allowance of addr to Uniswap V3 Router
+    allowance = weth_contract.functions.allowance(addr, UNISWAP_V3_ROUTER).call()
+    return (
+        f"WETH allowance to Uniswap V3 Router (address: {addr}):\n"
+        f"{allowance / 10**18} WETH\n"
+    )
+
+if __name__ == "__main__":
+    import asyncio
+    print(asyncio.run(get_balances()))
